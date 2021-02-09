8db500a0ce7cf4c36a82f2a2b9d3f3a90f508351
Jiachi Liu <inbox@huozhi.im>
2021-02-10 00:08:49 +0800

    example: update to module css

--- example/README.md
+++ example/README.md
@@ -4,6 +4,16 @@
 
 ```sh
 yarn
-yarn tunnel
 yarn dev
 ```
+
+To expose your network to the internet, try any tunnel tool you can leverage.
+
+e.g. use `ngrok`
+
+```
+npm install -g ngrok
+ngork http 3000
+```
+
+Then visit the https url returned from ngrok instead of localhost:3000
--- example/pages/icon.image.js
+++ example/pages/icon.image.js
@@ -1,8 +1,8 @@
+import styles from './icon.image.module.css'
+
 export default function IconImage({title}) {
   return (
-    <div 
-      style={{textAlign: 'center', height: 100, fontSize: 36, color: '#fff', background: '#000', padding: '10px 40px'}}
-    >
+    <div className={styles.root}>
       <h1 style={{margin: 0}}>Imagegen {title}</h1>
     </div>
   )
