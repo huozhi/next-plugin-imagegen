ccf0301cf8a185b21d12d09d53b6e3da29932e4e
Jiachi Liu <inbox@huozhi.im>
2021-03-11 18:48:56 +0800

    fix: queries should be decoded

--- example/pages/api/imagegen/[...imagegen].js
+++ example/pages/api/imagegen/[...imagegen].js
@@ -1,9 +1,15 @@
-import { handler /* provider as defaultProvider */} from 'next-plugin-imagegen'
+import { handler, provider as defaultProvider } from 'next-plugin-imagegen'
 import { provider } from 'next-plugin-imagegen-puppeteer'
 
+const options = {
+  viewport: {
+    width: 1200,
+    height: 630,
+  },
+}
+
 export default handler(
-  provider({
-    type: 'jpeg',
-  })
-  /* defaultProvider() */
+  process.env.IMAGEGEN_MICROLINK ?
+    defaultProvider(options) :
+    provider(options)
 )
--- example/pages/index.js
+++ example/pages/index.js
@@ -1,8 +1,19 @@
+import { useState } from 'react'
+import styles from './index.module.css'
+
+const getImagePath = (text) => `/icon.image?title=${encodeURIComponent(text)}`
+
 export default function Index() {
+  const [title, setTitle] = useState(`T'he Id'ea`)
+  const [query, setQuery] = useState(getImagePath(title))
   return (
-    <div>
+    <div className={styles.root}>
       <h1>Generate Image</h1>
-      <img src='/icon.image?title=456' width={200} height={100} />
+      <div>
+        <input value={title} onChange={(e) => setTitle(e.target.value)} />
+        <button onClick={() => setQuery(getImagePath(title))}>Generate</button>
+      </div>
+      <img src={query} width={640} height={320} />
     </div>
   )
 }
